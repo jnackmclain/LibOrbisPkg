0cce1b3a001c853776079a65fc3f3199402d512d
kiwidoggie <forums@allenthinks.com>
2024-10-18 14:00:52 -0400

    Create fresh project.
    Move older .NET 8 source to _oldsrc
    
    Move 010 templates to _templates

---
+++ OrbisPkg/OrbisPkg.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace OrbisPkg.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ _oldsrc/LibOrbisPkg.UI/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace LibOrbisPkg.UI.Views
+{
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
